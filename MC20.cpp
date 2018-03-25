10e6adcc494967b30a53e76a4a02c11f646ffcdb
Radu - Eosif Mihailescu <radu.mihailescu@linux360.ro>
2018-03-25 00:15:05 +0000

    UPDATE: line framing and URC detection done.

--- MC20.cpp
+++ MC20.cpp
@@ -38,14 +38,98 @@
  * THE SOFTWARE.
  */
 
+#include <stdbool.h>
+#include <stddef.h>
+#include <string.h>
+
 #include "MC20.h"
 
-#define MC20_CR 0x0D
+const char MC20_URC_AMO[] PROGMEM = "ALARM MODE";
+const char MC20_URC_ARG[] PROGMEM = "ALARM RING";
+const char MC20_URC_CRY[] PROGMEM = "Call Ready";
+const char MC20_URC_MCN[] PROGMEM = "MO CONNECTED";
+const char MC20_URC_MRN[] PROGMEM = "MO RING";
+const char MC20_URC_NPD[] PROGMEM = "NORMAL POWER DOWN";
+const char MC20_URC_OVO[] PROGMEM = "OVER_VOLTAGE POWER DOWN";
+const char MC20_URC_OVW[] PROGMEM = "OVER_VOLTAGE WARNING";
+const char MC20_URC_RDY[] PROGMEM = "RDY";
+const char MC20_URC_RNG[] PROGMEM = "RING";
+const char MC20_URC_SRY[] PROGMEM = "SMS Ready";
+const char MC20_URC_UVO[] PROGMEM = "UNDER_VOLTAGE POWER DOWN";
+const char MC20_URC_UVW[] PROGMEM = "UNDER_VOLTAGE WARNING";
+
+/* Note that "CME ERROR" is not in this list because it's not an *U*RC. */
+const char MC20_URC_P_CBCM[] PROGMEM = "CBCM";
+const char MC20_URC_P_CBM[] PROGMEM = "CBM";
+const char MC20_URC_P_CCINFO[] PROGMEM = "CCINFO";
+const char MC20_URC_P_CCWA[] PROGMEM = "CCWA";
+const char MC20_URC_P_CDS[] PROGMEM = "CDS";
+const char MC20_URC_P_CFUN[] PROGMEM = "CFUN";
+const char MC20_URC_P_CGEV[] PROGMEM = "CGEV";
+const char MC20_URC_P_CGREG[] PROGMEM = "CGREG";
+const char MC20_URC_P_CLIP[] PROGMEM = "CLIP";
+const char MC20_URC_P_CMWT[] PROGMEM = "CMWT";
+const char MC20_URC_P_CMT[] PROGMEM = "CMT";
+const char MC20_URC_P_CMTI[] PROGMEM = "CMTI";
+const char MC20_URC_P_COLP[] PROGMEM = "COLP";
+const char MC20_URC_P_CPIN[] PROGMEM = "CPIN";
+const char MC20_URC_P_CREG[] PROGMEM = "CREG";
+const char MC20_URC_P_CRING[] PROGMEM = "CRING";
+const char MC20_URC_P_CSQN[] PROGMEM = "CSQN";
+const char MC20_URC_P_FPLMN[] PROGMEM = "FPLMN";
+const char MC20_URC_P_QBAND[] PROGMEM = "QBAND";
+const char MC20_URC_P_QCGTIND[] PROGMEM = "QCGTIND";
+const char MC20_URC_P_QGURC[] PROGMEM = "QGURC";
+const char MC20_URC_P_TSMSINFO[] PROGMEM = "TSMSINFO";
+
+/* Keep these two sorted in strcmp() order. It's easier to achieve that if
+ * they're already listed in sorted order above.
+ */
+PGM_P const MC20_Simple_URCs[] PROGMEM = {
+    MC20_URC_AMO,
+    MC20_URC_ARG,
+    MC20_URC_CRY,
+    MC20_URC_MCN,
+    MC20_URC_MRN,
+    MC20_URC_NPD,
+    MC20_URC_OVO,
+    MC20_URC_OVW,
+    MC20_URC_RDY,
+    MC20_URC_RNG,
+    MC20_URC_SRY,
+    MC20_URC_UVO,
+    MC20_URC_UVW
+};
+
+PGM_P const MC20_Plus_URCs[] PROGMEM = {
+    MC20_URC_P_CBCM,
+    MC20_URC_P_CBM,
+    MC20_URC_P_CCINFO,
+    MC20_URC_P_CCWA,
+    MC20_URC_P_CDS,
+    MC20_URC_P_CFUN,
+    MC20_URC_P_CGEV,
+    MC20_URC_P_CGREG,
+    MC20_URC_P_CLIP,
+    MC20_URC_P_CMWT,
+    MC20_URC_P_CMT,
+    MC20_URC_P_CMTI,
+    MC20_URC_P_COLP,
+    MC20_URC_P_CPIN,
+    MC20_URC_P_CREG,
+    MC20_URC_P_CRING,
+    MC20_URC_P_CSQN,
+    MC20_URC_P_FPLMN,
+    MC20_URC_P_QBAND,
+    MC20_URC_P_QCGTIND,
+    MC20_URC_P_QGURC,
+    MC20_URC_P_TSMSINFO
+};
 
 bool MC20::begin(bool goOnAir) {
     /* If the Arduino API wouldn't be the stinky mess it is, there would be an
      * interface that both SoftwareSerial and HardwareSerial implement and so
-     * MC20::_port could be of that type and we could write the elegant code
+     * MC20::port could be of that type and we could write the elegant code
      * below to test for availability. But it is and so we cannot.
      */
 //   if(!this->_port) {
@@ -53,6 +137,12 @@ bool MC20::begin(bool goOnAir) {
 //        * whether their initialization is complete or not.*/
 //       return false;
 //   }
+    /* In general, we expect the modem to reply in less than 300ms, according to
+     * the datasheet. Allowing three times that to account for stuff like
+     * multiprocessing-related interrupts.
+     */
+    this->port.setTimeout(1000);
+    /* Turn the modem power on. */
     if(this->vbat_pin != MC20_VBAT_PIN_HW) {
         pinMode(this->vbat_pin, OUTPUT);
     }
@@ -71,11 +161,9 @@ bool MC20::begin(bool goOnAir) {
     delay(700);
     digitalWrite(this->pkey_pin, LOW);
 
-    while(!this->established) {
-        this->sendCommand(F("AT"));
-    }
-    // Somewhere at the end:
-    this->established = true;
+    while(!(this->established = this->challengeResponse("AT", "OK")));
+//TODO: configure
+//TODO: obey goOnAir
     return true;
 }
 
@@ -91,9 +179,157 @@ void MC20::sendCommand(const char *command) {
         return;
     }
     (void) this->port.write(command);
-    (void) this->port.write((uint8_t)MC20_CR);
-    this->port.flush();
+    this->sendEOL();
 }
 
 void MC20::sendCommand(const __FlashStringHelper *command) {
+    /* command is a pointer to EEPROM so a value of NULL is actually valid. */
+    if(!this->established) {
+        return;
+    }
+    PGM_P cmd = reinterpret_cast<PGM_P>(command);
+    while(uint8_t c = pgm_read_byte(cmd++)) {
+        (void) this->port.write(c);
+    }
+    this->sendEOL();
+}
+
+void MC20::sendEOL(void) {
+    /* Datasheet says CR alone will suffice. */
+    (void) this->port.write((uint8_t)'\r');
+    this->port.flush();
+}
+
+bool MC20::challengeResponse(const char *challenge, const char *response) {
+    this->sendCommand(challenge);
+    /* Possibly wait for and read the first line in. */
+    if(!this->recvFilteredLine()) {
+        /* Timeout ocurred, report failure. */
+        return false;
+    }
+    if(!strcmp(this->lastLine, response)) {
+        /* The first line read was the response, we're done. */
+        return true;
+    } else if(!strcmp(this->lastLine, challenge)) {
+        /* The first line was the challenge echoed back, go on. */
+        if(!this->recvFilteredLine()) {
+            /* Timeout ocurred, report failure. */
+            return false;
+        }
+        if(!strcmp(this->lastLine, response)) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        /* The first line was neither the challenge, nor the response. */
+        return false;
+    }
+}
+
+bool MC20::recvFilteredLine(void) {
+    while(true) {
+        MC20::RecvRawLineStatus result = this->recvRawLine();
+        switch(result) {
+            case MC20::MC20_RRL_COMPLETE:
+                if(this->maybeProcessURC()) {
+                    /* This was an URC, we need the next line. */
+                    continue;
+                } else {
+                    return true;
+                }
+                break;
+            case MC20::MC20_RRL_EMPTY:
+                continue;
+                break;
+            case MC20::MC20_RRL_PARTIAL:
+                if(this->recvRawLine(true) != MC20::MC20_RRL_COMPLETE) {
+                    /* We cannot get a complete line in 2 attempts, bail. */
+                    return false;
+                }
+                break;
+            case MC20::MC20_RRL_NOTHING:
+                /* Timeout ocurred, report failure. */
+                return false;
+                break;
+        }
+    }
+}
+
+MC20::RecvRawLineStatus MC20::recvRawLine(bool retry) {
+    char *start = this->lastLine;
+    size_t bufSize = sizeof(this->lastLine) / sizeof(this->lastLine[0]) - 1;
+    size_t lastSize = strlen(this->lastLine);
+
+    if(retry && lastSize) {
+        start += lastSize;
+        bufSize -= lastSize;
+    }
+    size_t lineLen = this->port.readBytesUntil('\r', start, bufSize);
+    /* Stream::readBytesUntil() is not well-behaved in terms of C-strings. */
+    start[lineLen] = '\0';
+    /* If this was a well-formed line, the next character in the serial buffer
+     * would have to be a '\n'.
+     */
+    if(this->port.peek() == '\n') {
+        (void) this->port.read();
+        if(lineLen) {
+            /* At least one character was read before EOL. */
+            return MC20::MC20_RRL_COMPLETE;
+        } else {
+            return MC20::MC20_RRL_EMPTY;
+        }
+    } else {
+        if(this->port.peek() == -1) {
+            if(lineLen) {
+                /* Timeout ocurred mid-line. */
+                return MC20::MC20_RRL_PARTIAL;
+            } else {
+                return MC20::MC20_RRL_NOTHING;
+            }
+        } else {
+            /* We've exceeded our available buffer. */
+            return MC20::MC20_RRL_OVERFLOW;
+        }
+    }
+}
+
+int MC20::flashStringCompare(const void *key, const void *candidate) {
+    /* Extract candidate first ... */
+    PGM_VOID_P candidateValue = pgm_read_ptr(candidate);
+    /* ... then do the comparison. */
+    return strcmp_P((const char *)key, (const char *)candidateValue);
+}
+
+bool MC20::maybeProcessURC(void) {
+    PGM_VOID_P urcMatch;
+
+    if(this->lastLine[0] == '+') {
+        char *urcEnd = strchr(this->lastLine, ':');
+        if(!urcEnd) {
+            /* Begins with + but doesn't have a : down the line, not an URC. */
+            return false;
+        }
+        urcEnd[0] = '\0';
+        /* Note how having to have the strings declared and thus stored before
+         * the arrays of pointers to them in EEPROM saves us from bsearch()
+         * returning NULL to mean "the first element in the array matches".
+         */
+        urcMatch = bsearch(&lastLine[1], MC20_Plus_URCs,
+                           sizeof(MC20_Plus_URCs) / sizeof(MC20_Plus_URCs[0]),
+                           sizeof(MC20_Plus_URCs[0]), MC20::flashStringCompare);
+        urcEnd[0] = ':';
+    } else {
+        urcMatch = bsearch(
+            lastLine, MC20_Simple_URCs,
+            sizeof(MC20_Simple_URCs) / sizeof(MC20_Simple_URCs[0]),
+            sizeof(MC20_Simple_URCs[0]), MC20::flashStringCompare);
+    }
+
+    if(urcMatch) {
+        //TODO: process URC.
+        return true;
+    } else {
+        return false;
+    }
 }
--- MC20.h
+++ MC20.h
@@ -42,6 +42,7 @@
 #define _MC20_H_INCLUDED
 
 #include <stdbool.h>
+#include <stdint.h>
 
 #include <Stream.h>
 #if defined(ARDUINO) && ARDUINO >= 100
@@ -53,6 +54,8 @@
 #define MC20_VBAT_PIN_WIO 7
 #define MC20_VBAT_PIN_HW -1
 #define MC20_PKEY_PIN_WIO 13
+#define MC20_DTR_PIN_WIO 9
+#define MC20_DTP_PIN_HW -1
 
 class MC20
 {
@@ -70,11 +73,14 @@ class MC20
          *             powered in your application.
          *   powerKeyPin - pin which controls the PWRKEY input of the MC20 to
          *                 turn it on.
+         *   dtrPin - pin which controls the DTR input of the MC20, used in low
+         *            power scenarios to control wake-up. Pass MC20_DTP_PIN_HW
+         *            to signal that you don't want this functionality.
          */
         MC20(Stream &serialPort, int8_t vbatPin=MC20_VBAT_PIN_WIO, int8_t
-             powerKeyPin=MC20_PKEY_PIN_WIO):
+             powerKeyPin=MC20_PKEY_PIN_WIO, int8_t dtrPin=MC20_DTR_PIN_WIO):
              port(serialPort), vbat_pin(vbatPin), pkey_pin(powerKeyPin),
-             established(false) {};
+             dtr_pin(dtrPin), established(false) {};
 
         /*
          * Description:
@@ -94,6 +100,10 @@ class MC20
          *             installed, whether it's PIN-locked or not and various
          *             other SIM- and operator-side settings, this may result in
          *             the MC20 registering with the GSM network on power on.
+         * NOTE: this is NOT equivalent to a hardware RF-Kill switch! The modem
+         *       has plenty time to perform RF operations between the moment
+         *       it's powered on and until the command corresponding to the
+         *       goOnAir value is sent.
          */
         bool begin(bool goOnAir);
 
@@ -106,8 +116,16 @@ class MC20
         void end(void);
     private:
         Stream &port;
-        int8_t vbat_pin, pkey_pin;
+        int8_t vbat_pin, pkey_pin, dtr_pin;
         bool established;
+        char lastLine[256];
+        enum RecvRawLineStatus {
+            MC20_RRL_COMPLETE,
+            MC20_RRL_EMPTY,
+            MC20_RRL_PARTIAL,
+            MC20_RRL_NOTHING,
+            MC20_RRL_OVERFLOW
+        };
 
         /*
          * Description:
@@ -115,8 +133,82 @@ class MC20
          *   EOL character (which triggers the command's execution) and blocking
          *   until the entire command line has been sent.
          */
+        /* This is Arduino String, not std::string! */
         void sendCommand(const String &command);
         void sendCommand(const char *command);
         void sendCommand(const __FlashStringHelper *command);
+
+        /*
+         * Description:
+         *   Sends the command execution character and flushes the port.
+         */
+        void sendEOL(void);
+
+        /*
+         * Description:
+         *   Sends challenge and then looks for response alone on the first line
+         *   of input, optionally preceeded by an echo of challenge and/or URCs.
+         *   Returns true if response was found and false otherwise. If the
+         *   input is initially empty, it obeys the timeout set with
+         *   Stream::setTimeout() on the port. If response was not found as
+         *   described anove or the port's timeout was hit, returns false.
+         */
+        bool challengeResponse(const char *challenge, const char *response);
+        bool challengeResponse(const __FlashStringHelper *challenge,
+                               const char *response);
+        bool challengeResponse(const char *challenge,
+                               const __FlashStringHelper *response);
+        bool challengeResponse(const __FlashStringHelper *challenge,
+                               const __FlashStringHelper *response);
+
+        /*
+         * Description:
+         *   Reads characters from the input and stores in MC20::lastLine until
+         *   "\r\n" is seen. The final "\r\n" is not stored in MC20::lastLine.
+         *   If the input is initially empty, it obeys the timeout set with
+         *   Stream::setTimeout() on the port. Regardless of the outcome,
+         *   MC20::lastLine is properly maintained as a C string.
+         * Returns:
+         *   MC20::MC20_RRL_COMPLETE if a complete line was found and stored in
+         *                           MC20::lastLine.
+         *   MC20::MC20_RRL_EMPTY if only a "\r\n" was read.
+         *   MC20::MC20_RRL_PARTIAL if at least one character (different from
+         *                          '\r') was read within the port's timeout.
+         *                          Call the function again with a true argument
+         *                          to (attempt to) finish the line.
+         *   MC20::MC20_RRL_NOTHING if no characters arrived within the port's
+         *                          timeout.
+         *   MC20::MC20_RRL_OVERFLOW if available (remaining) space in
+         *                           this->lastLine is insufficient to buffer
+         *                           the entire line the MC20 sent.
+         * NOTE: calling MC20::recvRawLine() a second time with a true argument
+         *       and getting a MC20::MC20_RRL_NOTHING return is a good sign the
+         *       MC20 is stuck and not coming back without a power cycle.
+         */
+        MC20::RecvRawLineStatus recvRawLine(bool retry=false);
+
+        /*
+         * Description:
+         *   Reads complete lines from the input, filtering out (and acting
+         *   upon) any URCs encountered. Saves the first non-URC line
+         *   encountered in MC20::lastLine and returns true. If no usable
+         *   (non-empty and non-URC) line was read within the port's timeout,
+         *   returns false.
+         */
+        bool recvFilteredLine(void);
+
+        /*
+         * Description:
+         *   Checks MC20::lastLine to see if it's an URC, acts upon it if so and
+         *   returns true. Returns false if MC20::lastLine was not an URC.
+         */
+        bool maybeProcessURC(void);
+
+        /*
+         * Description:
+         *   Serves as comparison function for bsearch() for PROGMEM-stored
+         *   arrays of strings.
+         */
+        static int flashStringCompare(const void *key, const void *candidate);
 };
 #endif
